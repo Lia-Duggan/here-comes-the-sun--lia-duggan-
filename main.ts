8de49d59fcfe2a4494a377a472b3e23664df2692
Lia-Duggan <92882948+Lia-Duggan@users.noreply.github.com>
2021-10-25 12:49:34 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,33 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # . # . #
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . # . #
+        . # # # .
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        # . # . #
+        . # # # .
+        # # # # #
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # . # . #
+        . # # # .
+        # # # # #
+        . # # # .
+        # . # . #
+        `)
+})
